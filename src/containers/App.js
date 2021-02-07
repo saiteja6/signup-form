d7e7713c735642b03d974bb8d1cdfac076bbd8fd
saiteja6 <bendalamsaiteja6@gmail.com>
2021-02-07 10:21:58 +0530

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import Signup from './Signup'
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Navbar /> */}
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Signup.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+    
+    render() {
+        return (
+            <div>
+                {/* <form onSubmit={}>
+                    <input 
+                        type="text"
+                        placeholder="Name"
+                        name=""
+                        value={}
+                        onChange={}
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Name"
+                        name=""
+                        value={}
+                        onChange={}
+                    />
+                </form> */}
+                <h1>Hello, World!</h1>            
+            </div>
+        )
+    }
+}

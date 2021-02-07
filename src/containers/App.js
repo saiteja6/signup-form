7a809cded50a7dca2c114d59fcf4833433a1bd15
saiteja6 <bendalamsaiteja6@gmail.com>
2021-02-07 12:48:24 +0530

    react-router added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,13 @@
 import '../App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Signup from './Signup'
+
+
 
 function App() {
   return (
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react'
 
 export default class Signup extends Component {
+
+    state = {
+        name: "",
+        email: "",
+        password: ""
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target    
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+    }
     
     render() {
         return (
             <div>
-                {/* <form onSubmit={}>
+                <form 
+                    className="SignupForm"
+                    onSubmit={this.handleSubmit}>
+                    <label> Name
                     <input 
                         type="text"
-                        placeholder="Name"
-                        name=""
-                        value={}
-                        onChange={}
+                        placeholder="Name.."
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
                     />
+                    </label>
+                    <label> E-mail
                     <input 
                         type="text"
-                        placeholder="Name"
-                        name=""
-                        value={}
-                        onChange={}
+                        placeholder="E-mail.."
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
                     />
-                </form> */}
-                <h1>Hello, World!</h1>            
+                    </label>
+                    <label> Password
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    </label>
+                    <button>SignUp</button>
+                </form>
+                {/* <h1>Hello, World!</h1>    */}
+                <h5>{this.state.name}</h5>
+                <h5>{this.state.email}</h5>         
+                <h5>{this.state.password}</h5>         
             </div>
         )
     }
